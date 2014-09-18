7a47bb0f45842efb817da8e8d2e3052229bcb304
Mikola Lysenko <mikolalysenko@gmail.com>
2014-09-18 18:27:38 -0500

    adding test cases for orientation

--- test/test.js
+++ test/test.js
@@ -4,6 +4,8 @@ var tape = require("tape")
 var surfaceNets = require("../surfacenets")
 var ndarray = require("ndarray")
 var fill = require("ndarray-fill")
+var det = require("robust-determinant")
+var gamma = require("gamma")
 
 tape("sphere-test", function(t) {
   var shape = []
@@ -28,6 +30,11 @@ tape("sphere-test", function(t) {
     size *= 16
     fact *= d
 
+    if(d !== 3) {
+      continue
+    }
+    
+
     var x = ndarray(new Float32Array(size), shape)
     fill(x, funcs[d-1])
     var surf = surfaceNets(x, 36)
@@ -38,6 +45,33 @@ tape("sphere-test", function(t) {
         u += Math.pow(p[j]-8,2)
       }
       t.ok(Math.abs(u-36) <= 1.2, "test: " + d + "d - " + u)
+    }
+
+    if(d > 1) {
+      //Compute volume of surface
+      var vol = 0.0
+      for(var k=0; k<surf.cells.length; ++k) {
+        var c = surf.cells[k]
+        var m = []
+        for(var i=0; i<c.length; ++i) {
+          var pp = surf.positions[c[i]].slice()
+          for(var j=0; j<d; ++j) {
+            pp[j] -= 8
+          }
+          m.push(pp)
+        }
+        var r = det(m)
+        t.ok(r[r.length-1] >= -1e-6, "check orientation: " + r)
+        vol += r[r.length-1] / gamma(d+1)
+      }
+      var bvol = Math.pow(Math.PI, Math.floor(d/2)) * Math.pow(6.0, d)
+      if((d % 2) === 0) {
+        bvol /= gamma((d/2) + 1)
+      } else {
+        var kk = Math.floor(d/2)
+        bvol *= Math.pow(2.0, d) * gamma(1+kk) / gamma(1+d)
+      }
+      t.ok(Math.abs(vol / bvol - 1.0) < 0.05, "check volume - vol=" + vol + " vs bvol=" + bvol)
     }
   }
   t.end()
